735193045bc388f049353ed9e30a5093408dd5ff
garou404 <charlesmeyer347@gmail.com>
2025-02-10 15:47:40 +0100

    graph generation correction, queries added to the class JenaSPARQLExecutor, command line interface advancements

--- java_project/src/itsudparis/application/JenaSPARQLExecutor.java
+++ java_project/src/itsudparis/application/JenaSPARQLExecutor.java
@@ -15,12 +15,13 @@ public class JenaSPARQLExecutor {
     public JenaSPARQLExecutor(String pathToData, String owlRuleFile, String ruleFile, String rdfFile) {
         // Load RDF model
     	Model model = JenaEngine.readModel(pathToData+rdfFile);
+    	NS = model.getNsPrefixURI("");
     	Model owlInferencedModel =
     			JenaEngine.readInferencedModelFromRuleFile(model, pathToData+owlRuleFile);    	
         // Apply inference rules
         this.inferedModel = 
         		JenaEngine.readInferencedModelFromRuleFile(owlInferencedModel, pathToData+ruleFile);
-      //NS = baseModel.getNsPrefixURI("");
+      
         this.prefix = "PREFIX ns: <http://www.semanticweb.org/charles-m/ontologies/2025/1/untitled-ontology-22#>\n"
 				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
 				+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n";
@@ -127,4 +128,141 @@ public class JenaSPARQLExecutor {
                 "?player ns:nationality ?natio }";
         return executeQuery(query);
     }
+    
+    public String listTeams() {
+        String query = "SELECT ?team_name WHERE { " +
+                "?team rdf:type ns:Team . " +
+                "?team ns:teamName ?team_name . }";
+        return executeQuery(query);
+    }
+
+    public String listRegionsOfTeams() {
+        String query = "SELECT DISTINCT ?region WHERE { " +
+                "?team rdf:type ns:Team . " +
+                "?team ns:region ?region . }";
+        return executeQuery(query);
+    }
+
+    public String listTeamsPerRegion() {
+        String query = "SELECT ?team_name ?region WHERE { " +
+                "?team rdf:type ns:Team . " +
+                "?team ns:teamName ?team_name . " +
+                "?team ns:region ?region . }";
+        return executeQuery(query);
+    }
+
+    public String listTeamsAndPlayers() {
+        String query = "SELECT ?team_name ?player_name WHERE { " +
+                "?team rdf:type ns:Team . " +
+                "?team ns:teamName ?team_name . " +
+                "?team ns:hasRoster ?roster . " +
+                "?roster ns:rosterYear 2025 . " +
+                "?roster ns:hasPlayer ?player . " +
+                "?player ns:summonerName ?player_name . }";
+        return executeQuery(query);
+    }
+
+    public String listPlayersAndTeams() {
+        String query = "SELECT ?player_name ?team_name\n"
+        		+ "WHERE {\n"
+        		+ "	?team rdf:type ns:Team .\n"
+        		+ "	?team ns:teamName ?team_name .\n"
+        		+ "	?team ns:hasRoster ?roster .\n"
+        		+ "	?roster ns:rosterYear 2025 .\n"
+        		+ "	?roster ns:hasPlayer ?player .\n"
+        		+ "	?player ns:summonerName ?player_name .\n"
+        		+ "}";
+        return executeQuery(query);
+    }
+
+    public String listPlayersByRegionAndNationality() {
+        String query = "SELECT ?player_name ?natio ?region ?team_name WHERE { " +
+                "?team rdf:type ns:Team . " +
+                "?team ns:teamName ?team_name . " +
+                "?team ns:region ?region . " +
+                "?team ns:hasRoster ?roster . " +
+                "?roster ns:rosterYear 2025 . " +
+                "?roster ns:hasPlayer ?player . " +
+                "?player ns:summonerName ?player_name ; " +
+                "ns:nationality ?natio . }";
+        return executeQuery(query);
+    }
+
+    public String listTeamsAPlayerPlayedFor(String player) {
+        String query = "SELECT ?player ?team_name ?year WHERE { " +
+                "?team rdf:type ns:Team . " +
+                "?team ns:teamName ?team_name . " +
+                "?team ns:hasRoster ?roster . " +
+                "?roster ns:hasPlayer ?player . " +
+                "?roster ns:rosterYear ?year . " +
+                "?player ns:summonerName \"" + player + "\" . }";
+        return executeQuery(query);
+    }
+
+    public String listChampionsAndStats() {
+        String query = "SELECT ?champion_name ?HP ?attack_damage ?attack_speed ?armor ?magic_resist ?movement_speed ?resource_type WHERE { " +
+                "?champion rdf:type ns:Champion ; " +
+                "ns:championName ?champion_name ; " +
+                "ns:healthPoints ?HP ; " +
+                "ns:attackDamage ?attack_damage ; " +
+                "ns:attackSpeed ?attack_speed ; " +
+                "ns:armor ?armor ; " +
+                "ns:magicResist ?magic_resist ; " +
+                "ns:movementSpeed ?movement_speed ; " +
+                "ns:resourceType ?resource_type . }";
+        return executeQuery(query);
+    }
+
+    public String listChampionsAndFactions() {
+        String query = "SELECT ?champion_name ?faction WHERE { " +
+                "?champion rdf:type ns:Champion ; " +
+                "ns:championName ?champion_name ; " +
+                "ns:hasFaction ?faction . }";
+        return executeQuery(query);
+    }
+
+    public String listChampionsAndPlayers() {
+        String query = "SELECT ?champion_name ?player WHERE { " +
+                "?champion rdf:type ns:Champion ; " +
+                "ns:championName ?champion_name ; " +
+                "ns:isPlayedBy ?player . }";
+        return executeQuery(query);
+    }
+    
+    public String listRoles() {
+        String query = "SELECT ?role_name WHERE { " +
+                "?champion rdf:type ns:Role ; " +
+                "ns:roleName ?role_name . }";
+        return executeQuery(query);
+    }
+
+    public String listPlayersByRole(String role) {
+        String query = "SELECT ?player_name WHERE { " +
+                "?player rdf:type ns:Player ; " +
+                "ns:summonerName ?player_name ; " +
+                "ns:playsRole ?role . " +
+                "?role ns:roleName \"" + role + "\" . }";
+        return executeQuery(query);
+    }
+
+    public String listChampionsByRole(String role) {
+        String query = "SELECT ?champion_name WHERE { " +
+                "?champion rdf:type ns:Champion ; " +
+                "ns:championName ?champion_name ; " +
+                "ns:hasPrimaryRole ?role . " +
+                "?role ns:roleName \"" + role + "\" . }";
+        return executeQuery(query);
+    }
+    
+    public String getNationalityCount() {
+        String query = "SELECT ?nationality (COUNT(?player) AS ?count)\n"
+        		+ "WHERE {\n"
+        		+ "	?player rdf:type ns:Player .\n"
+        		+ "	?player ns:nationality ?nationality .\n"
+        		+ "}\n"
+        		+ "GROUP BY ?nationality\n"
+        		+ "ORDER BY DESC(?count)";
+        return executeQuery(query);
+    }
+
 }
--- java_project/src/itsudparis/application/Main.java
+++ java_project/src/itsudparis/application/Main.java
@@ -6,6 +6,7 @@
 package itsudparis.application;
 import itsudparis.application.JenaSPARQLExecutor;
 import java.io.File;
+import java.util.Scanner;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.PropertyConfigurator;
@@ -22,7 +23,176 @@ public class Main {
 
 	public static void main(String[] args) {
 		JenaSPARQLExecutor executor = new JenaSPARQLExecutor("../graph/", "owlrules.txt", "rule.txt", "knowledge_graph_project.rdf");
-		System.out.println(executor.getChampionsByPlayer("Vladi"));
+		Scanner new_scanner = new Scanner(System.in);
+		System.out.println("   _    ___   _      _____   __  __  ____      ______  ______ ");
+		System.out.println("  / /  / __/ / \\    / ___/  / / / / / __/     / __  / / ____/ ");
+		System.out.println(" / /_ / __/ / _ \\  / / ___ / /_/ / / __/     / /_/ / / ___/   ");
+		System.out.println("/___//___/ /_/ \\_\\ \\__/_/ /_____/ /___/     /_____/ /_/       ");  
+
+		System.out.println("   _    ___   _____   ____  __  __  _____   ______ ");
+		System.out.println("  / /  / __/ / ___/  / __/ / / / / / __  | / ____/ ");
+		System.out.println(" / /_ / __/ / / ___ / __/ /  \\/ / / /_/ / /___  /  ");
+		System.out.println("/___//___/  \\__/_/ /___/ /_/\\ _/ /_____/ /_____/   ");
+		System.out.println("\nWelcome to the League of Legends Interactive Encyclopedia!\n"
+				+ "\n"
+				+ "League of Legends (LoL) is a fast-paced, competitive Multiplayer Online Battle Arena \n(MOBA) game where two teams of five players face off on Summoner’s Rift. Each \nplayer controls a unique Champion, using strategy, teamwork, and skill to \ndestroy the enemy Nexus and claim victory.\n");
+		while (true) {
+			System.out.println("About which topic would you like to know?\n"
+					+ "\n"
+					+ "1 => Team – Learn about competitive teams, their rosters.\n"
+					+ "2 => Player – Explore information on famous players, their teams, their champions.\n"
+					+ "3 => Champion – Discover details about champions, their abilities, and lore.\n"
+					+ "4 => Role – Understand different in-game roles like Top, Jungle, Mid, ADC, and Support.");
+			
+			System.out.println("\nType a number to begin your journey into the world of League of Legends!\n>");
+			//System.out.println(executor.test());
+			String topic_index = new_scanner.nextLine();
+			switch (topic_index) {
+			case "1": // TEAM - TEAM - TEAM - TEAM - TEAM - TEAM - TEAM - TEAM - TEAM
+				System.out.println("Choose a question index among those listed below\n");
+				System.out.println(
+						"1 => List teams that exist\n" // V
+						+ "2 => List different regions of teams\n" // V
+						+ "3 => List the teams per region\n" // V
+						+ "4 => List teams and their players\n" // V
+						+ "5 => List the different nationalities in a team\n" // V
+						+ "6 => List all players that play for a team\n" // V
+						+ "7 => List every players that play for a team with year\n"); // V
+				String team_question_index = new_scanner.nextLine();
+				String team = "";
+				String year = "";
+				switch (team_question_index) {
+				case "1":
+	                System.out.println(executor.listTeams());
+	                break;
+	            case "2":
+	                System.out.println(executor.listRegionsOfTeams());
+	                break;
+	            case "3":
+	                System.out.println(executor.listTeamsPerRegion());
+	                break;
+	            case "4":
+	                System.out.println(executor.listTeamsAndPlayers());
+	                break;
+	            case "5":
+	            	System.out.println("type the name of a team: ");
+	            	team = new_scanner.nextLine();
+	                System.out.println(executor.getNationalitiesOfTeam(team));
+	                break;
+	            case "6":
+	            	System.out.println("type the name of a team: ");
+	            	team = new_scanner.nextLine();
+	                System.out.println(executor.getPlayersOfTeamWithoutYear(team));
+	                break;
+	            case "7":
+	            	System.out.println("type the name of a team: ");
+	            	team = new_scanner.nextLine();
+	            	System.out.println("\ntype a year (2024 or 2025)");
+	            	year = new_scanner.nextLine();
+	                System.out.println(executor.getPlayersOfTeamWithYear(team));
+	                break;
+	            default:
+	                System.out.println("");
+	        }
+				break;
+			case "2": // PLAYER - PLAYER - PLAYER - PLAYER - PLAYER - PLAYER - PLAYER - PLAYER
+				System.out.println("Choose a question index among those listed below\n");
+				System.out.println(
+						"1 => List players and their team\n" // V
+						+ "2 => List players of a region and their nationality\n" // V
+						+ "3 => List champions mostly played by a player\n" // V
+						+ "4 => List team of a player\n" // V
+						+ "5 => List every team a player played for\n" // V
+						+ "6 => List teammates of a player\n"
+						+ "7 => List the nationality count of each player"); // V
+				String player_question_index = new_scanner.nextLine();
+				String player = "";
+				switch (player_question_index) {
+				case "1":
+	                System.out.println(executor.listPlayersAndTeams());
+	                break;
+	            case "2":
+	                System.out.println(executor.listPlayersByRegionAndNationality());
+	                break;
+	            case "3":
+	            	System.out.println("type the name of a player: ");
+	            	player = new_scanner.nextLine();
+	                System.out.println(executor.getChampionsByPlayer(player));
+	                break;
+	            case "4":
+	            	System.out.println("type the name of a player: ");
+	            	player = new_scanner.nextLine();
+	                System.out.println(executor.getTeammatesOfPlayer(player));
+	                break;
+	            case "5":
+	            	System.out.println("type the name of a player: ");
+	            	player = new_scanner.nextLine();
+	                System.out.println(executor.listTeamsAPlayerPlayedFor(player));
+	                break;
+	            case "6":
+	            	System.out.println("type the name of a player: ");
+	            	player = new_scanner.nextLine();
+	                System.out.println(executor.getTeammatesOfPlayer(player));
+	                break;
+	            case "7":
+	            	System.out.println(executor.getNationalityCount());
+	            	break;
+	            default:
+	                System.out.println("");
+	        }
+				break;
+			case "3": // CHAMPION - CHAMPION - CHAMPION - CHAMPION - CHAMPION - CHAMPION
+				System.out.println("Choose a question index among those listed below\n");
+				System.out.println(
+						"1 => List all champions and their stats\n" // V
+						+ "2 => List all champions and their factions\n" // V
+						+ "3 => List all champions and the players that play them\n");
+				String champion_question_index = new_scanner.nextLine();
+				switch (champion_question_index) {
+				case "1":
+	                System.out.println(executor.listChampionsAndStats());
+	                break;
+	            case "2":
+	                System.out.println(executor.listChampionsAndFactions());
+	                break;
+	            case "3":
+	                System.out.println(executor.listChampionsAndPlayers());
+	                break;
+	            default:
+	                System.out.println("");
+	        }
+				break;
+			case "4":// ROLE - ROLE - ROLE - ROLE - ROLE - ROLE - ROLE - ROLE
+				System.out.println("Choose a question index among those listed below\n");
+				System.out.println(
+						"1 => List the five roles\n"
+						+ "2 => List player that plays the role\n"
+						+ "3 => List champions of a role\n");
+				String role_question_index = new_scanner.nextLine();
+				String role = "";
+				switch (role_question_index) {
+				case "1":
+	                System.out.println(executor.listRoles());
+	                break;
+	            case "2":
+	            	System.out.println("type a role: ");
+	            	role = new_scanner.nextLine();
+	                System.out.println(executor.listPlayersByRole(role));
+	                break;
+	            case "3":
+	            	System.out.println("type a role: ");
+	            	role = new_scanner.nextLine();
+	                System.out.println(executor.listChampionsByRole(role));
+	                break;
+	            default:
+	                System.out.println("");
+	        }
+				break;
+			default:
+				System.out.println("Sorry, wrong number\n");
+			}
+		}
 	}
 }
+
 
